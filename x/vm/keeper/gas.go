f734e473f42b2fa0608cc3066f67ffbd992ddb31
Konstantin Munichev <toobwn@gmail.com>
2025-09-02 14:09:22 +0200

    Refund gas to the actual fee payer
    Authored by rkollar

--- x/vm/keeper/gas.go
+++ x/vm/keeper/gas.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/core"
@@ -44,8 +45,21 @@ func (k *Keeper) RefundGas(ctx sdk.Context, msg core.Message, leftoverGas uint64
 		// positive amount refund
 		refundedCoins := sdk.Coins{sdk.NewCoin(denom, sdkmath.NewIntFromBigInt(remaining))}
 
-		// refund to sender from the fee collector module account, which is the escrow account in charge of collecting tx fees
-		err := k.bankWrapper.SendCoinsFromModuleToAccount(ctx, authtypes.FeeCollectorName, msg.From.Bytes(), refundedCoins)
+		// try to use tx fee payer to refund the correct account
+		var feePayer []byte
+		v := ctx.Value("fee-payer")
+		if v != nil {
+			acc, ok := v.(sdk.AccAddress)
+			if !ok {
+				panic(fmt.Sprintf("incorrect fee-payer type: %T", v))
+			}
+			feePayer = acc.Bytes()
+		} else {
+			feePayer = msg.From.Bytes()
+		}
+
+		// refund to fee payer from the fee collector module account, which is the escrow account in charge of collecting tx fees
+		err := k.bankWrapper.SendCoinsFromModuleToAccount(ctx, authtypes.FeeCollectorName, feePayer, refundedCoins)
 		if err != nil {
 			err = errorsmod.Wrapf(errortypes.ErrInsufficientFunds, "fee collector account failed to refund fees: %s", err.Error())
 			return errorsmod.Wrapf(err, "failed to refund %d leftover gas (%s)", leftoverGas, refundedCoins.String())
