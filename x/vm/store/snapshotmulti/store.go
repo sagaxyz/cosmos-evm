fa42727e2a96a7125f8561f3a635d28c836529af
Facundo <facundomedica@gmail.com>
2025-11-06 12:05:54 +0100

    remove debug

--- x/vm/store/snapshotmulti/store.go
+++ x/vm/store/snapshotmulti/store.go
@@ -3,7 +3,6 @@ package snapshotmulti
 import (
 	"fmt"
 	"io"
-	"runtime/debug"
 	"sort"
 
 	"github.com/cosmos/evm/x/vm/store/snapshotkv"
@@ -116,7 +115,7 @@ func (s *Store) CacheMultiStoreWithVersion(_ int64) (storetypes.CacheMultiStore,
 func (s *Store) GetStore(key storetypes.StoreKey) storetypes.Store {
 	store := s.stores[key]
 	if key == nil || store == nil {
-		panic(fmt.Sprintf("kv store with key %v has not been registered in stores, stack 1: %s", key, string(debug.Stack())))
+		panic(fmt.Sprintf("kv store with key %v has not been registered in stores", key))
 	}
 	return store.CurrentStore()
 }
@@ -125,7 +124,7 @@ func (s *Store) GetStore(key storetypes.StoreKey) storetypes.Store {
 func (s *Store) GetKVStore(key storetypes.StoreKey) storetypes.KVStore {
 	store := s.stores[key]
 	if key == nil || store == nil {
-		panic(fmt.Sprintf("kv store with key %v has not been registered in stores, stack 2: %s", key, string(debug.Stack())))
+		panic(fmt.Sprintf("kv store with key %v has not been registered in stores", key))
 	}
 	return store.CurrentStore()
 }
